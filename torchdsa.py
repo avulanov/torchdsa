92248326e7971adff26b5ae0c6278ab00bb2aae7
Aleksandr Ulanov <avulanov@gmail.com>
2024-10-20 22:30:33 -0700

    Update stack

--- torchdsa.py
+++ torchdsa.py
@@ -2,17 +2,30 @@ import torch
 from torch import Tensor
 
 class TorchStack:
-    def __init__(self, max_size: int, batch_size: int) -> None:
-        self.array = torch.zeros([max_size, batch_size])
+    def __init__(self, shape: torch.Size, max_size: int) -> None:
+        array_shape = torch.Size([max_size]) + shape
+        self.array = torch.zeros(array_shape)
         self.max_size = max_size
-        self.batch_size = batch_size
+        self.expected_shape = shape
         self.index = 0
+        self.batches = []
+        print(array_shape)
+
     def push(self, data: Tensor) -> None:
-        assert(data.shape[-1] == self.batch_size)
-        self.array[self.index : self.index + 1:, :self.batch_size] = data
+        assert((data.shape[1:]) == self.expected_shape)
+        batch_size = data.shape[0]
+        assert(self.index + batch_size <= self.max_size)
+        self.array[self.index : self.index + batch_size, :] = data
+        self.batches.append(batch_size)
+        self.index += batch_size
+        print(self.array)
+
     def pop(self,) -> Tensor:
-        res = self.array[self.index : self.index + 1, :]
-        self.index -= 1
+        assert(self.index > 0)
+        prev_batch_size = self.batches.pop()
+        res = self.array[self.index - prev_batch_size : self.index, :]
+        self.index -= prev_batch_size
         return res
+    
     def isEmpty(self) -> bool:
         return self.index == 0
